f4d3470bf00e9f0bc4f793a24d45a6caf9b462f7
skuukzky14 <idhamdummy1@gmail.com>
2022-08-05 22:04:59 +0700

    1. Add Search by Get Product 2. Add Filter By Price Get Product 3. Add Pagination Get Product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,10 +1,25 @@
 const { Product } = require('../models')
 
 const getProduct=async(req,res)=>{
+    const { searchby, keyword, filterBy,min,max, page } = req.query
+    let products
+    let searchQuery = searchby==="recommendation"?{[searchby]:keyword==="true"?true:false}:{[searchby]:{$regex:new RegExp(keyword)}}
+    let minPrice = +min || 0
+    let maxPrice = +max || Infinity
+    let offset = +page || 1
     try {
-        const products = await Product.find()
+        if(searchby){
+            products = await Product.find(searchQuery)
+            console.log('searchby', searchQuery)
+        }
+        else if(filterBy){
+            products = await Product.find({price:{$gte:minPrice, $lte:maxPrice}})
+        }else{
+            products = await Product.find().limit(8).skip(offset>1?(offset-1)*8:0)
+        }
         res.status(200).json({message:'hello product', data:products})
     } catch (error) {
+        console.log(error)
         res.status(500).json({message:error})
     }
 }
@@ -20,9 +35,9 @@ const getProductById=async(req,res)=>{
 }
 
 const createProduct=async(req,res)=>{
-    const { name, detail, thumbnail, recommendation, discountId, images, deleted } = req.body
+    const { name, detail, thumbnail, recommendation, price, discountId, images, deleted } = req.body
     const dataProduct = {
-        name, detail, thumbnail, recommendation, discountId, images, deleted 
+        name, detail, thumbnail, recommendation, price,  discountId, images, deleted 
     }
     try {
         const addProduct = await Product.create(dataProduct)
@@ -46,7 +61,6 @@ const editProduct=async(req,res)=>{
 
 const deleteProduct=async(req,res)=>{
     const { id } = req.params 
-    
     try {
         const deletedProduct = await Product.findByIdAndDelete(id)
         res.status(401).json({message:'delete success', deletedProduct:deletedProduct})    
